7d5d9d43693c277032c1b1c798f632b1373f025d
vsantos33 <98541899+vsantos33@users.noreply.github.com>
2022-01-27 19:09:30 +0100

    creacion componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ListadoPersonaComponent } from './listado-persona/listado-persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    FooterComponent,
+    HeaderComponent,
+    ListadoPersonaComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/listado-persona/listado-persona.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-persona',
+  templateUrl: './listado-persona.component.html',
+  styleUrls: ['./listado-persona.component.css']
+})
+export class ListadoPersonaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
